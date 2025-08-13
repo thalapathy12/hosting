ab499903b5230020b148e325eaa08a4af50e1670
thalapathy12 <84682589+thalapathy12@users.noreply.github.com>
2025-08-13 12:12:21 +0530

    Update leadRoutes.js

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -6,11 +6,13 @@ const {
   updateLeadStatus,
   convertToClient
 } = require('../controllers/leadController');
-const { protect, authorizeRoles } = require('../middleware/authMiddleware');
-
-router.post('/create', protect, authorizeRoles('Admin', 'SuperAdmin'), createLead);
-router.get('/', protect, authorizeRoles('Admin', 'SuperAdmin', 'Client'), getAllLeads);
-router.patch('/:id/status', protect, authorizeRoles('Admin', 'SuperAdmin'), updateLeadStatus);
-router.post('/:id/convert', protect, authorizeRoles('Admin', 'SuperAdmin'), convertToClient);
+const { authenticate } = require('../middleware/authMiddleware');
+const { authorizeRoles} = require('../middleware/roleMiddleware');
+router.post('/create', authenticate, authorizeRoles('Admin', 'Super_admin'), createLead);
+router.get('/', authenticate, authorizeRoles('Admin', 'Super_admin', 'Member'), getAllLeads);
+router.patch('/:id/status', authenticate, authorizeRoles('Admin', 'Super_admin'), updateLeadStatus);
+router.post('/:id/convert', authenticate, authorizeRoles('Admin', 'Super_admin'), convertToClient);
 
 module.exports = router;
+
+
