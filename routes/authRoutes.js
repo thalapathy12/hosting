dcd8b3188d8edad045122193201f51a623da4e48
thalapathy12 <84682589+thalapathy12@users.noreply.github.com>
2025-08-13 11:50:55 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {
-  register,
-  login,
-  forgotPassword,
-  resetPassword,
-  getAllUsersWithRoleStats,
-  getUsersByRole ,
-  superAdminDashboard,
-  adminDashboard,
-  clientDashboard
-} = require('../controllers/authController');
+const authController = require('../controllers/authController');
+const { authenticate } = require('../middleware/authMiddleware');
+const { authorizeRoles } = require('../middleware/roleMiddleware');
 
-const { protect, authorizeRoles } = require('../middleware/authMiddleware');
-
-
-router.post('/register', register);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:token', resetPassword);
-router.get('/users',protect,authorizeRoles('SuperAdmin'),getAllUsersWithRoleStats);
-router.get('/users-by-role', protect, authorizeRoles('SuperAdmin'), getUsersByRole);
-
-
-router.get('/super-admin/dashboard', protect, authorizeRoles('SuperAdmin'), superAdminDashboard);
-router.get('/admin/dashboard', protect, authorizeRoles('Admin'), adminDashboard);
-router.get('/client/dashboard', protect, authorizeRoles('Client'), clientDashboard);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
+router.get('/id/:id', authenticate, authController.getUserById);
+router.get('/name/:name', authenticate, authController.getUsersByName);
+router.get('/role/:role', authenticate, authorizeRoles('Super_admin', 'Admin'), authController.getUsersByRole);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password/:token', authController.resetPassword);
 
 module.exports = router;
