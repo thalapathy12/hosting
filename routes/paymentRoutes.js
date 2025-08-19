bee484862886182af80b8dac42cdef3086ee1419
vijayavignesh-R <vijayavignesh561@gmail.com>
2025-08-19 16:09:05 +0530

    Add files via upload

---
+++ routes/paymentRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const paymentController = require('../controllers/paymentController');
+
+router.post('/',paymentController.createPayment);
+router.get('/',paymentController.getPayments);
+router.put('/:id',paymentController.updatePayments);
+router.delete('/:id',paymentController.deletePayment);
+
+module.exports = router;
