2744f813fa67eb61a94272353cdb8cc3cfb8a446
thalapathy12 <84682589+thalapathy12@users.noreply.github.com>
2025-08-25 15:40:59 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const paymentController = require('../controllers/paymentController');
 
-router.post('/',paymentController.createPayment);
+router.post('/',authenticate, authorizeRoles('Admin', 'Super_admin'),paymentController.createPayment);
 router.get('/',paymentController.getPayments);
 router.put('/:id',paymentController.updatePayments);
 router.delete('/:id',paymentController.deletePayment);
+
 
 module.exports = router;
