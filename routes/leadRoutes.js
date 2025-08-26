1458e1d2a373d3d4222990954e7066fd05c8f100
thalapathy12 <84682589+thalapathy12@users.noreply.github.com>
2025-08-26 17:26:58 +0530

    Update leadRoutes.js

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -5,7 +5,8 @@ const {
   getAllLeads,
   updateLeadStatus,
   convertToClient,
-  deleteClient
+  deletelead,
+  updatelead
 } = require('../controllers/leadController');
 const { authenticate } = require('../middleware/authMiddleware');
 const { authorizeRoles} = require('../middleware/roleMiddleware');
@@ -13,8 +14,8 @@ router.post('/create', authenticate, authorizeRoles('Admin', 'Super_admin'), cre
 router.get('/', authenticate, authorizeRoles('Admin', 'Super_admin', 'Member'), getAllLeads);
 router.patch('/:id/status', authenticate, authorizeRoles('Admin', 'Super_admin'), updateLeadStatus);
 router.post('/:id/convert', authenticate, authorizeRoles('Admin', 'Super_admin'), convertToClient);
-router.delete('/:id', authenticate, authorizeRoles('Admin', 'Super_admin'), deleteClient);
-
+router.delete('/:id', authenticate, authorizeRoles('Admin', 'Super_admin'), deletelead);
+router.put('/:id', authenticate, authorizeRoles('Admin', 'Super_admin'), updatelead);
 module.exports = router;
 
 
